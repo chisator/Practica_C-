7ad86c25c4f2426cdd21513b925c4b3e52ae621a
chisator <gianfrancosacchi@hotmail.com.ar>
2025-05-19 10:51:00 -0300

    Agregar archivos de proyecto.

---
+++ TipoParcial2/CEjecutora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TipoParcial2
+{
+    public class CEjecutora
+    {
+        public static void Main()
+        {
+            List<CPaquete> listaPaquetes = new List<CPaquete>();
+            CPaquete p1 = new CPaquete(101, "Bariloche – 10 días y 8 noches – Bus – Hotel Pucón – Pensión Completa");
+            p1.SetCosto(8978.50f);
+            listaPaquetes.Add(p1);
+
+            CPaquete p2 = new CPaquete(202, "Mendoza – 7 días y 5 noches – Avión – Hotel Sol – Media Pensión");
+            p2.SetCosto(10500.00f);
+            listaPaquetes.Add(p2);
+
+            CPaquete p3 = new CPaquete(303, "Cataratas – 5 días y 4 noches – Bus – Hotel Selva – Desayuno");
+            p3.SetCosto(7500.00f);
+            listaPaquetes.Add(p3);
+
+            CPaquete p4 = new CPaquete(404, "Salta – 8 días y 6 noches – Avión – Hotel Norte – Pensión Completa");
+            p4.SetCosto(9500.00f);
+            listaPaquetes.Add(p4);
+            Boolean continuar = true;
+            //1) Solicitar impuesto
+            Console.WriteLine("Ingrese impuesto único:");
+            float impuesto = float.Parse(Console.ReadLine());
+            CPaquete.SetImpuesto(impuesto);
+            // 2) Carga iterativa
+            Console.WriteLine("Ingrese el número de paquete:");
+            ushort num_paquete = ushort.Parse(Console.ReadLine());
+            while (num_paquete != 0)
+            {    
+                Console.WriteLine("Ingrese la descripción del paquete:");
+                string? detalle = Console.ReadLine();
+                CPaquete paquete = new CPaquete(num_paquete, detalle);
+                Console.WriteLine("Ingrese el costo del paquete:");
+                paquete.SetCosto(float.Parse(Console.ReadLine()!));
+                Console.WriteLine("Paquete ingresado --> " + paquete.DarDatos());
+                listaPaquetes.Add(paquete);
+                Console.Write("\nIngrese Nro. de Paquete (0 para finalizar): ");
+                num_paquete = ushort.Parse(Console.ReadLine());
+            }
+            if(listaPaquetes.Count == 0)
+            {
+                Console.WriteLine("No se ingresaron paquetes turísticos válidos.");
+                return;
+            }
+            CPaquete masBarato = listaPaquetes.OrderBy(p => p.DarPrecio()).First();
+            Console.WriteLine($"PAQUETE MAS ECONOMICO : {masBarato.DarDatos()}");
+            
+            return;
+        }
+    }
+}
---
+++ TipoParcial2/CPaquete.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoParcial2
+{
+    internal class CPaquete
+    {
+        private static float impuesto;
+        public static float Impuesto
+        {
+            get => impuesto;
+            set { 
+                if(value < 0)
+                    throw new ArgumentException("EL IMPUESTO NO PUEDE SER NEGATIVO");
+                impuesto = value;
+            } 
+        }
+        private ushort num_Paquete;
+        public ushort Num_Paquete
+        {
+            get => num_Paquete;
+            set => num_Paquete = value;
+        }
+        private string? detalle;
+        public string? Detalle
+        {
+            get => detalle;
+            set
+            {
+                    if (value == null|| value=="")
+                        throw new ArgumentException("EL DETALLE NO PUEDE SER VACIO");
+                    detalle = value;
+            }
+        }
+        private float costo;
+        public float Costo
+        {
+            get => costo;
+            set => costo = value;
+        }
+        public static void SetImpuesto(float valor)
+        {
+            impuesto = valor;
+        }
+        public static float GetImpuesto()
+        {
+            return impuesto;
+        }
+        public void SetCosto(float valor)
+        {
+            costo = valor;
+        }
+        public float GetCosto()
+        {
+            return costo;
+        }
+        public float DarPrecio()
+        {
+            return Costo + (Costo * Impuesto / 100);
+        }
+        public float DarPrecio(sbyte cuotas)
+        {
+            float recargo = (cuotas - 1) * 0.10f;
+            return DarPrecio() * (1 + recargo);
+        }
+        public string DarDatos()
+        {
+            return $"NUMERO DE PAQUETE {num_Paquete} " +
+                $"DESCRIPCION {detalle} " +
+                $"COSTO {costo} " +
+                $"IMPUESTO {impuesto} " +
+                $"VALOR FINAL {DarPrecio()}";
+        } 
+        public bool EsMásBaratoQue(CPaquete otroPaquete)
+        {
+            return this.DarPrecio() < otroPaquete.DarPrecio();            
+        }
+        public CPaquete(ushort num_paquete,string detalle)
+        {
+            if (detalle == null || detalle == "")
+                throw new ArgumentException("EL DETALLE NO PUEDE SER VACIO");
+            Num_Paquete = num_paquete;
+            Detalle = detalle;
+        }
+    }
+}
