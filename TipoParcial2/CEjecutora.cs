d6440c635d63b9c68b0b4d981eaf63ed2c333f3b
chisator <gianfrancosacchi@hotmail.com.ar>
2025-05-19 10:51:00 -0300

    Agregar archivos de proyecto.

---
+++ TipoParcial2/CEjecutora.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TipoParcial2
+{
+    public class CEjecutora
+    {
+        private static float impuesto;
+        public static float Impuesto
+        {
+            get => impuesto;
+            set=> impuesto = value;
+        }
+        public static void SetImpuesto(float valor)
+        {
+            impuesto = valor;
+        }
+        public static float GetImpuesto()
+        {
+            return impuesto;
+        }
+        public CEjecutora()
+        {
+            
+        }
+    }
+}
---
+++ TipoParcial2/CPaquete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoParcial2
+{
+    internal class CPaquete
+    {
+    }
+}
